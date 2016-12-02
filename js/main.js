54f7569f6101385f130896a216c33dfe91d17a3b
mlindtsugarfree <mlindtsugarfree@gmail.com>
2016-12-02 10:38:38 +0200

    changes for  FMI

--- js/main.js
+++ js/main.js
@@ -1,3 +1,29 @@
-/**
- * Created by zhivko.terziev on 18.10.2016 г..
- */
+function loadMovieData(id, title, type, year, plot) {
+
+    var basePath = 'http://www.omdbapi.com/?';
+
+    $.ajax({
+        method: "GET",
+        url: basePath + 'i=' + id + '&t=' + title + '&type=' + type + '&y=' + year + '&plot=' + plot,
+        dataType: "json"
+    }).done(function (response) {
+
+        console.log(response);
+
+        var temp = $('#temp').clone();
+        temp.css("display", "block");
+
+        temp.find('#tempTitle').text(response.Title.toString());
+        temp.find('#tempReleased').html(response.Released.toString());
+        temp.find('#tempGenre').html(response.Genre.toString());
+
+        $('#movies').append(temp);
+
+        console.log('done!')
+    }).fail(function () {
+        console.log('fail!')
+    }).always(function () {
+        console.log('always!')
+    })
+
+}
